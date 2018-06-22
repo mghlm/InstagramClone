cd662baacdf3595afabca3699adfed022308bf48
Magnus Holm <mail@magnusholm.com>
2018-06-22 13:41:43 +0100

    Add anchor extensions to UIView

---
+++ InstagramClone/Utils/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  InstagramClone
+//
+//  Created by magnus holm on 22/06/2018.
+//  Copyright © 2018 magnus holm. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+

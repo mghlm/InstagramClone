99d5ec3d04dff0a63c1c8dc54f537e514dd8f7a5
Magnus Holm <mail@magnusholm.com>
2018-07-10 20:54:40 +0100

    Add logo image

--- InstagramClone/Login/LoginViewController.swift
+++ InstagramClone/Login/LoginViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let logoContainerView: UIView = {
+        let view = UIView()
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        logoImageView.contentMode = .scaleAspectFill
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 50)
+        logoImageView.anchor(centerX: view.centerXAnchor, centerY: view.centerYAnchor)
+        view.backgroundColor = UIColor.rgb(red: 0, green: 120, blue: 175)
+        return view
+    }()
+    
     let signUpButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Don't have an account? Sign up", for: .normal)
@@ -22,12 +33,20 @@ class LoginViewController: UIViewController {
         navigationController?.pushViewController(signUpController, animated: true)
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         view.backgroundColor = .white
         
         view.addSubview(signUpButton)
         signUpButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        
+        navigationController?.isNavigationBarHidden = true 
         
         
     }

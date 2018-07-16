4ee1c7f33a13121bca5304a63567c4ead75532a5
Magnus Holm <mail@magnusholm.com>
2018-07-16 17:49:49 +0100

    Reset UI when user signs up with new account

--- InstagramClone/Login/SignUpController.swift
+++ InstagramClone/Login/SignUpController.swift
@@ -169,6 +169,11 @@ class SignUpController: UIViewController, UIImagePickerControllerDelegate, UINav
                         }
                         
                         print("Successfully saved user info int DB")
+                        
+                        guard let mainTabBarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController else { return }
+                        mainTabBarController.setupViewControllers()
+                        
+                        self.dismiss(animated: true, completion: nil)
                     })
                 })
             })
